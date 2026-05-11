162605205e8eefb939c8f44fec361fc35b56b99d
Laura Valentina Rodriguez Rodriguez <laura.rodriguez10valentina@gmail.com>
2026-05-10 20:21:38 -0500

    Clase pantalon y clase falda creadas-Laura Valentina Rodriguez

---
+++ src/main/java/gabriela/tienda/clases falda y pantalon/Pantalon.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gabriela.tienda.model;
+
+import gabriela.tienda.interfaces.Vendible;
+
+public class Pantalon extends Producto implements Vendible {
+    public Pantalon(String nombre, int cantidad, String color, double precio, String talla) {
+        super(nombre, cantidad, color, precio, talla);
+    }
+
+    @Override
+    public String getTipoProducto() { return "Pantalón"; }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println(nombre + " (" + color + ", talla " + talla + ") - " 
+                           + cantidad + " disponibles - $" + precio);
+    }
+
+    @Override
+    public void vender(int cantidad) {
+        if (cantidad <= this.cantidad) {
+            this.cantidad -= cantidad;
+            System.out.println("Se vendieron " + cantidad + " pantalones. Quedan " + this.cantidad + ".");
+        } else {
+            System.out.println("No hay suficiente stock.");
+        }
+    }
+}
